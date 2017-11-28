ea5f8166de7d71ce9af84d0dc8ea46157bb82ed5
GabrielCUNY <gabriel.kidcuny@gmail.com>
2017-11-28 10:01:09 +0100

    C'est genial

--- MatriceNote.py
+++ MatriceNote.py
@@ -7,20 +7,22 @@ don_Movie = pd.read_csv("ml-latest-small/movies.csv")
 UserId = [elt for elt in don_ratings.userId]
 MovieId = [elt for elt in don_ratings.movieId]
 Rating = [elt for elt in don_ratings.rating]
-M_IdMovie = [elt for elt in don_Movie.movieId]
+M_IdMovie = [elt for elt in don_Movie.movieId] #Tout mes film par id
 M_Film_User_Note = [[None for j in range(len(M_IdMovie))] for i in range(UserId[-1]+1)] #Matrice global qui contien les notes (None si film pas note)
 M_id = [] #Matrice qui contient 671 vecteur (1 vecteur 1 personne) avec chaque note des film noter par l'user
-
-
-for i in range(671):
+j = 0
+UserId.append(672)
+for i in range(672):
     M_id.append([])
-    while UserId[i] == i+1:
-        M_id[i].append(Rating[i])
-
+    while UserId[j] == i:
+        M_id[i].append(Rating[j])
+        j += 1
+del M_id[0]
 
 def IdFilm(i):
     return M_IdMovie.index(i)
 
 for i in range(671):
-    for elt in M_idFix[i]:
-        M_Coeur[i][IdFilm(elt)].append(elt)
+    for elt in range(len(MovieId)):
+        M_Film_User_Note[i][IdFilm(MovieId[elt])] = Rating[elt]
+        print(Rating[elt])
