40766ecd6cc7432ebc8ae37872e052225fe851ab
GabrielCUNY <gabriel.kidcuny@gmail.com>
2017-11-28 10:47:51 +0100

    C'est genial

--- MatriceNote.py
+++ MatriceNote.py
@@ -9,20 +9,35 @@ MovieId = [elt for elt in don_ratings.movieId]
 Rating = [elt for elt in don_ratings.rating]
 M_IdMovie = [elt for elt in don_Movie.movieId] #Tout mes film par id
 M_Film_User_Note = [[None for j in range(len(M_IdMovie))] for i in range(UserId[-1]+1)] #Matrice global qui contien les notes (None si film pas note)
-M_id = [] #Matrice qui contient 671 vecteur (1 vecteur 1 personne) avec chaque note des film noter par l'user
+M_note = [] #Matrice qui contient 671 vecteur (1 vecteur 1 personne) avec chaque note des film noter par l'user
+M_id = []
+
 j = 0
 UserId.append(672)
 for i in range(672):
+    M_note.append([])
     M_id.append([])
     while UserId[j] == i:
-        M_id[i].append(Rating[j])
+        M_note[i].append(Rating[j])
+        M_id[i].append(MovieId[j])
         j += 1
+del M_note[0]
 del M_id[0]
+
+
 
 def IdFilm(i):
     return M_IdMovie.index(i)
 
-for i in range(671):
-    for elt in range(len(MovieId)):
-        M_Film_User_Note[i][IdFilm(MovieId[elt])] = Rating[elt]
-        print(Rating[elt])
+
+for i in range(len(M_note)):
+    for j in range(len(M_note[i])):
+        M_Film_User_Note[i+1][IdFilm(M_id[i][j])] = M_note[i][j]
+
+print(M_Film_User_Note[15][IdFilm(1682)])
+
+
+
+
+
+
