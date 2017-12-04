004f47694e21e73228a1748b09f879dbfcbc6963
GabrielCUNY <gabriel.kidcuny@gmail.com>
2017-12-04 10:20:34 +0100

    Le bon Dico.py

---
+++ Dico.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+from io import StringIO
+don_ratings = pd.read_csv("ml-latest-small/ratings.csv")
+X = np.asarray(don_ratings)
+don_Movie = pd.read_csv("ml-latest-small/movies.csv")
+UserId = [elt for elt in don_ratings.userId]
+MovieId = [elt for elt in don_ratings.movieId]
+Rating = [elt for elt in don_ratings.rating]
+M_IdMovie = [elt for elt in don_Movie.movieId]
+M_Film_User_Note = [[None for j in range(len(M_IdMovie))] for i in range(UserId[-1]+1)] #Matrice global qui contien les notes (None si film pas note)
+M_id = [] #Matrice qui contient 671 vecteur (1 vecteur 1 personne) avec chaque note des films noter par l'user
+
+
+dict_MatriceToFilm = {}
+dict_FilmtoMatrice = {}
+for IdMovie,value in enumerate(M_IdMovie,1): #film est l'indice du film
+    dict_FilmtoMatrice[IdMovie] = value
+    dict_MatriceToFilm[value] = IdMovie
+    #if film ==0:
+
+def index_movie(id_film):
+    return dict_MatriceToFilm[id_film]-1
+
+    #for elt in M_IdMovie:
+        #for elt1 in film:
+            #dict_MatriceToFilm[elt] = elt1
+            #dict_FilmtoMatrice[elt1] = elt
+
+
+
+#fonction prediction
+#IdFilm.head()
